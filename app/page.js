a86806de276394b40672beb7b0fb78e3229675ab
Okuku Itorobong <patrickokuku1@gmail.com>
2023-06-19 11:53:15 +0100

    home(mobile app) responsive design

--- app/page.js
+++ app/page.js
@@ -4,13 +4,13 @@ import ProjectList from "@/components/ProjectList";
 export default function Home() {
   // The Mobile App Page
   return (
-    <section className="p-[4.3%]">
-      <section className="flex_row items-center justify-between mb-11">
-        <div className="flex_row gap-4">
-          <h1 className="text-[46px] text-[#0D062D] font-semibold">
+    <section className="py-[6%] px-[45px] lg:p-[4.3%]">
+      <section className="flex_col lg:flex_row items-center lg:justify-between mb-11">
+        <div className="flex_row w-[100%] gap-4 mb-4 lg:m-0">
+          <h1 className="text-[30px] lg:text-[46px] text-[#0D062D] font-semibold">
             Mobile App
           </h1>
-          <div className="flex_row gap-2">
+          <div className="flex_row gap-2 ml-auto lg:ml-0">
             <Image
               src="/icons/arrow-square-up.svg"
               width={30}
@@ -86,7 +86,6 @@ export default function Home() {
               width={16}
               height={16}
               alt="arrow down"
-              color="#787486"
             ></Image>
           </div>
           <div className="flex_row items-center justify-evenly w-[122px] h-[40px] rounded-[6px] border-[1px] border-[#787486]">
@@ -102,13 +101,12 @@ export default function Home() {
               width={16}
               height={16}
               alt="arrow down"
-              color="#787486"
             ></Image>
           </div>
         </div>
 
         <div className="flex_row items-center gap-5">
-          <div className="flex_row items-center justify-evenly w-[97px] h-[40px] rounded-[6px] border-[1px] border-[#787486]">
+          <div className="flex_row items-center justify-evenly w-[97px] h-[40px] rounded-[6px] border-[1px] border-[#787486] text-[#787486]">
             <Image
               src="/icons/profile-2users.svg"
               width={16}
@@ -118,14 +116,24 @@ export default function Home() {
             <p>Share</p>
           </div>
 
-          <div className="border-[1px] h-[28px] bg-[#787486]" />
+          <div className="hidden lg:block border-[1px] h-[28px] bg-[#787486]" />
 
-          <Image src="/icons/list.svg" width={40} height={40}></Image>
-          <Image src="/icons/grid.svg" width={21} height={21}></Image>
+          <Image
+            className="hidden lg:block"
+            src="/icons/list.svg"
+            width={40}
+            height={40}
+          ></Image>
+          <Image
+            className="hidden lg:block"
+            src="/icons/grid.svg"
+            width={21}
+            height={21}
+          ></Image>
         </div>
       </section>
 
-      <section className="flex_row gap-4">
+      <section className="flex_row flex-wrap gap-4">
         <ProjectList name="To Do" />
         <ProjectList name="On Progress" />
         <ProjectList name="Done" />
