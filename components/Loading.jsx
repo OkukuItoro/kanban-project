8cf51ae5ffda0085f72abb0ca615ecb020cc9a20
Okuku Itorobong <patrickokuku1@gmail.com>
2023-06-20 04:08:15 +0100

    loading screen and animation

---
+++ components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="w-[100vw] h-[100vh] flex_row items-center justify-center">
+      <div className="loading_animate  w-[100px] h-[100px] rounded-full border-[4px] border-l-[#5030B5] border-[#9683EF]"></div>
+    </div>
+  );
+};
+
+export default Loading;
